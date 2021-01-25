c7e20ff1eef7e67bb1a744f4b9fb04c63c698fef
salimhs <74917442+salimhs@users.noreply.github.com>
2021-01-25 12:51:11 -0500

    photon.js_ver0.1

---
+++ photon.js
@@ -0,0 +1,52 @@
+class Photon {
+
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(-c, 0);
+    this.history = [];
+    this.stopped = false;
+    this.theta = 0;
+  }
+
+  stop() {
+    this.stopped = true;
+  }
+
+  update() {
+    if (!this.stopped) {
+      this.history.push(this.pos.copy());
+      const deltaV = this.vel.copy();
+      deltaV.mult(dt);
+      this.pos.add(deltaV);
+    }
+
+    if (this.history.length > 500) {
+      this.history.splice(0, 1);
+    }
+
+
+  }
+
+  show() {
+
+  
+    strokeWeight(4);
+    // REFACTORING ASPECT, changed trail colour 
+    stroke(112,128,144);
+    //  REFACTORING ASPECT, called image function
+    image(img,this.pos.x -20  ,this.pos.y - 15 , 50,50);
+
+
+    strokeWeight(2);
+    noFill();
+    beginShape();
+    for (let v of this.history) {
+      vertex(v.x,v.y)
+    }
+
+    endShape();
+
+  }
+
+
+}
